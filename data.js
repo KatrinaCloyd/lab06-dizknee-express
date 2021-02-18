de9515e9a65711fab53865fb739ff96e75979e18
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-02-18 15:09:03 -0800

    name query in URL working

---
+++ data.js
@@ -0,0 +1,105 @@
+const characters = [
+    {
+        name: 'Ariel',
+        species: 'mythical being',
+        role: 'hero',
+        unique_power: 'enchanting singing voice',
+        movie: 'The Little Mermaid',
+        movie_year: 1989,
+        hand_drawn: true,
+        image: 'https://lumiere-a.akamaihd.net/v1/images/b_disneyprincess_updates_mobile_ariel_19273_ca84d9d1.jpeg?region=0,0,640,709',
+        gif: 'https://www.readunwritten.com/wp-content/uploads/ariel-gif.gif'
+    },
+    {
+        name: 'Flounder',
+        species: 'talking animal',
+        role: 'side kick',
+        unique_power: 'none',
+        movie: 'The Little Mermaid',
+        movie_year: 1989,
+        hand_drawn: true,
+        image: 'https://i.pinimg.com/originals/fe/fd/f4/fefdf4ea1b031541928c2df5fee74e5d.jpg',
+        gif: 'https://thumbs.gfycat.com/CheeryFluidClingfish-size_restricted.gif'
+    },
+    {
+        name: 'Ursula',
+        species: 'mythical being',
+        role: 'villan',
+        unique_power: 'sea magic and binding contracts',
+        movie: 'The Little Mermaid',
+        movie_year: 1989,
+        hand_drawn: true,
+        image: 'https://static.wikia.nocookie.net/disneyprincess/images/1/12/Ursula.jpg/revision/latest/top-crop/width/360/height/450?cb=20130102194736',
+        gif: 'https://i.imgur.com/3sTt25z.gif'
+    },
+    {
+        name: 'Snow White',
+        species: 'human',
+        role: 'hero',
+        unique_power: 'seeing the good in ANYONE',
+        movie: 'Snow White and the Seven Dwarfs',
+        movie_year: 1938,
+        hand_drawn: true,
+        image: 'https://i2-prod.mirror.co.uk/incoming/article12479954.ece/ALTERNATES/s615/0_Snow-White-and-the-Seven-Dwarfs.jpg',
+        gif: 'https://thumbs.gfycat.com/AdoredAgitatedAnkole-small.gif'
+    },
+    {
+        name: 'Evil Queen',
+        species: 'human',
+        role: 'villan',
+        unique_power: 'evil magic and an all knowing mirror',
+        movie: 'Snow White and the Seven Dwarfs',
+        movie_year: 1938,
+        hand_drawn: true,
+        image: 'https://static.wikia.nocookie.net/disney/images/8/89/Profile_-_The_Evil_Queen.jpeg/revision/latest?cb=20190312020700',
+        gif: 'https://i.pinimg.com/originals/0f/47/f8/0f47f8e27714ad84e087bbfd3ec5e45e.gif'
+    },
+    {
+        name: 'Pocahontas',
+        species: 'human',
+        role: 'hero',
+        unique_power: 'can paint with the colors of the wind',
+        movie: 'Pocahontas',
+        movie_year: 1995,
+        hand_drawn: true,
+        image: 'https://bluestockingorg.files.wordpress.com/2017/08/pocahontas1.jpg?w=636',
+        gif: 'https://media0.giphy.com/media/26tn2Zkhrg5Ja67fO/giphy.gif'
+    },
+    {
+        name: 'Nakoma',
+        species: 'human',
+        role: 'side kick',
+        unique_power: 'dissapproving and encouraging at the same time',
+        movie: 'Pocahontas',
+        movie_year: 1995,
+        hand_drawn: true,
+        image: 'https://static.wikia.nocookie.net/disney/images/4/42/Pocahontas-disneyscreencaps.com-1008.jpg/revision/latest/top-crop/width/300/height/300?cb=20121026213044',
+        gif: 'https://i.pinimg.com/originals/f1/2b/62/f12b62979660b9daabac55ee16abd056.gif'
+    },
+    {
+        name: 'Meeko',
+        species: 'animal',
+        role: 'side kick',
+        unique_power: 'eating',
+        movie: 'Pocahontas',
+        movie_year: 1995,
+        hand_drawn: true,
+        image: 'https://ohmy.disney.com/wp-content/uploads/2014/11/Meeko-eats-cookies.jpg',
+        gif: 'https://thumbs.gfycat.com/BlushingMilkyFoxterrier-size_restricted.gif'
+    },
+    {
+        name: 'Maleficent',
+        species: 'mythical being',
+        role: 'villan',
+        unique_power: 'curses and dragon form',
+        movie: 'Sleeping Beauty',
+        movie_year: 1959,
+        hand_drawn: true,
+        image: 'https://static.wikia.nocookie.net/disney/images/0/05/Profile_-_Maleficent.jpeg/revision/latest?cb=20190312020410',
+        gif: 'https://i.pinimg.com/originals/c2/ca/5b/c2ca5ba29bb47984ed28948850ed6d49.gif'
+    },
+];
+
+module.exports = {
+    characters
+};
--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000
+const { characters } = require('./data.js');
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/characters', (req, res) => {
+    res.json({ results: characters });
 })
+
+app.get('/characters/:name', (req, res) => {
+    const id = req.params.name;
+
+    const selectedCharacter =
+        characters.find((char) => char.name === id);
+
+    res.json({ results: selectedCharacter });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-console.log('hello?');
