dda70a624ae2b4d2aada590daf5a157920c6db53
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-02-18 15:28:01 -0800

    two tests passing

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -11,3 +11,24 @@ it('should respond with all character data', async (done) => {
     expect(response.body).toEqual(myData)
     done()
 })
+
+it('should respond with specific character data', async (done) => {
+    const myData = {
+        results: {
+            name: 'Ariel',
+            species: 'mythical being',
+            role: 'hero',
+            unique_power: 'enchanting singing voice',
+            movie: 'The Little Mermaid',
+            movie_year: 1989,
+            hand_drawn: true,
+            image: 'https://lumiere-a.akamaihd.net/v1/images/b_disneyprincess_updates_mobile_ariel_19273_ca84d9d1.jpeg?region=0,0,640,709',
+            gif: 'https://www.readunwritten.com/wp-content/uploads/ariel-gif.gif'
+        }
+    }
+    const response = await request.get('/characters/Ariel')
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(myData)
+    done()
+})
